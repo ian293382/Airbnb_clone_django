e7430b7d9253e33fb4a138603f1289a864f62467
Iannaan <moshou5571228@gmail.com>
2024-04-26 00:05:09 +0800

    User menu popup

---
+++ djangobnb/app/components/navbar/MenuLink.tsx
@@ -0,0 +1,24 @@
+'user client';
+
+
+interface MenuLinkProps {
+    label: string,
+    onClink: () => void;
+}
+
+const MenuLink: React.FC<MenuLinkProps> = ({
+    label,
+    onClink
+}) => {
+    return (
+        <div
+            onClick={onClink} 
+            className="px-5 py-4 cursor-pointer hover:bg-gray-100 transition">
+            {label}
+        </div>
+    )
+}
+
+
+
+export default MenuLink
--- djangobnb/app/components/navbar/UserNav.tsx
+++ djangobnb/app/components/navbar/UserNav.tsx
@@ -1,4 +1,13 @@
+'use client'
+
+import { useState } from 'react'; // store state it as a temporary variable 
+import MenuLink from './MenuLink';
+
+
 const UserNav = () => {
+    // isOpen 是狀態改變 Boolean  setIsOpen 是函式用來改變 isOpen狀態
+    const [isOpen, setIsOpen] = useState(true)
+
     return (
         <div className="p-2 relative inline-block border rounded-full">
             <button className="flex item-center">
@@ -11,8 +20,20 @@ const UserNav = () => {
                     <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z" />
                 </svg>
 
-
             </button>
+
+            {isOpen && (
+                <div className="w-[220px] absolute top-[60px] right-0 bg-white border rounded-md flex flex-col cursor-pointer">
+                    <MenuLink 
+                        label="Log in"
+                        onClink={()=> console.log('clicked')}
+                    />
+                    <MenuLink 
+                        label="sign up"
+                        onClink={()=> console.log('clicked')}
+                    />
+                </div>
+            )}
         </div>
     )
 }
