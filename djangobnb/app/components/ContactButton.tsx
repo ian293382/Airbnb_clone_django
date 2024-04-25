09685da15faeea5d54c145f6066ad0f5ea29fbdc
Iannaan <moshou5571228@gmail.com>
2024-04-25 10:09:08 +0800

    Landlord page (static)

---
+++ djangobnb/app/components/ContactButton.tsx
@@ -0,0 +1,9 @@
+const ContactButton = () => {
+    return (
+        <div className="mt-6 py-4 cursor-pointer px-6 bg-airbnb hover:bg-airbnb-dark transition text-white rounded-xl">
+            Contact
+        </div>
+    )
+}
+
+export default ContactButton;
