10f7dfab37e642a5df8197ce658d7ec7741fef21
Iannaan <moshou5571228@gmail.com>
2024-04-27 17:12:42 +0800

    setting Django MEDIA to transpoort to frontend show image

--- djangobnb/app/components/properties/PropertyList.tsx
+++ djangobnb/app/components/properties/PropertyList.tsx
@@ -3,8 +3,19 @@
 import { useEffect, useState } from 'react';
 import PropertyListItem from "./PropertyListitem";
 
+export type PropertyType = {
+    id: string;
+    title: string;
+    price_per_night: number;
+    image_url: string;
+
+}
+
 const PropertyList = () => {
-    const getProperties = async () => {
+
+    const [properties, setProperties] = useState<PropertyType[]>([]);
+
+    const getProperties = async () => {     
         const url = 'http://localhost:8000/api/properties/';
 
         await fetch(url,{
@@ -13,6 +24,8 @@ const PropertyList = () => {
             .then(response => response.json())
             .then((json)=> {
                 console.log('json',json)
+
+                setProperties(json.data)
             })
             .catch((error) => {
                 console.log('error',error);
@@ -25,10 +38,14 @@ const PropertyList = () => {
 
     return (
         <>
-            <PropertyListItem />
-            <PropertyListItem />
-            <PropertyListItem />
-            <PropertyListItem />
+            {properties.map((property) => {
+                return ( <PropertyListItem
+                            key={property.id}    
+                            property={property}
+                         />
+                )
+            })}
+        
         </>
         
     )
