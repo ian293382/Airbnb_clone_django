967d62bc3ad5713d5b39bd518160edcad278a9a3
Iannaan <moshou5571228@gmail.com>
2024-04-28 00:33:59 +0800

    setting Next.js signup form

--- djangobnb/app/components/model/SignupModel.tsx
+++ djangobnb/app/components/model/SignupModel.tsx
@@ -3,27 +3,70 @@
 import Model from "./Model"
 
 import { useState } from "react"
+import { useRouter } from "next/navigation"
 import useSignupModel from "@/app/hooks/useSignupModel"
 import CustomButton from "../forms/CustomButton"
+import apiService from "@/app/services/apiService"
 
 const SignupModel = () => {
+    //
+    // Variables
+
+    const router = useRouter();
     const SignupModel = useSignupModel()
+    const [email, setEmail] = useState('');
+    const [password1, setPassword1] = useState('');
+    const [password2, setPassword2] = useState('');
+    const [errors, setErrors] = useState<string[]>([]);
+
+    //
+    // Submit functionality
+    const sumbitSignup = async () => {
+        const formData ={
+            email: email,
+            password1: password1,
+            password2: password2,
+        }
+        const response = await apiService.post('/api/auth/register/', formData)
+       if (response.access){
+        // handleLogin
+        SignupModel.close();
+
+        router.push('/')
+       } else {
+            const tmpErrors: string[] = Object.values(response).map((error: any) => {
+                return error;
+            })
+            setErrors(tmpErrors);
+       }
+    }
 
     const  content = (
         <>
           
-            <form action="" className="space-y-4">
-                 <input placeholder="Your email address" type="email" className="w-full h-[54px] px-4 border border-gray-300 rounded-xl" />
-                 <input placeholder="Your password" type="password" className="w-full h-[54px] px-4 border border-gray-300 rounded-xl" />
+            <form
+                action={sumbitSignup}
+                className="space-y-4"
+            >
+                 <input onChange={(e) => setEmail(e.target.value)} placeholder="Your email address" type="email" className="w-full h-[54px] px-4 border border-gray-300 rounded-xl" />
+                 <input onChange={(e) =>setPassword1(e.target.value)} placeholder="Your password" type="password" className="w-full h-[54px] px-4 border border-gray-300 rounded-xl" />
 
-                 <input placeholder="Repeat your password" type="password" className="w-full h-[54px] px-4 border border-gray-300 rounded-xl" />
-
-                <div className="p-5 bg-airbnb text-white rounded-xl opacity-80">
-                    the error message
-                </div>
+                 <input  onChange={(e) => setPassword2(e.target.value)} placeholder="Repeat your password" type="password" className="w-full h-[54px] px-4 border border-gray-300 rounded-xl" />
+                {errors.map((error, index) => {
+                    return (
+                        <div
+                            key= {`error_${index}`}
+                            className="p-5 bg-airbnb text-white rounded-xl opacity-80"
+                        
+                        >
+                            {error}
+                        </div>
+                    )
+                })}
+             
                 <CustomButton 
                     label='Submit'
-                    onClick={ ()=> { console.log('submit test')}}
+                    onClick={sumbitSignup}
                 />
             </form>
         </>
