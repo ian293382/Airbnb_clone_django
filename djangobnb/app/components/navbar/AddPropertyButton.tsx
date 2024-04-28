9d81ec801b30dd109559aae8a9a05e12eee2f90d
Iannaan <moshou5571228@gmail.com>
2024-04-28 23:07:58 +0800

    import  addPropertyModel in AddPropertButtins

--- djangobnb/app/components/navbar/AddPropertyButton.tsx
+++ djangobnb/app/components/navbar/AddPropertyButton.tsx
@@ -1,6 +1,18 @@
+'use client';
+import useAddPropertyModel from "@/app/hooks/useAddPropertyModel";
+
 const AddPropertyButton = () => {
+    const addPropertyModel = useAddPropertyModel();
+
+    const airbnbYourHome =  () => {
+        addPropertyModel.open()
+    }
     return (
-        <div className="p-2 text-sm font-semibold rounded-full hover:bd-gray-200">
+        <div 
+            onClick={airbnbYourHome}  
+            className="p-2 cursor-pointer text-sm font-semibold rounded-full hover:bd-gray-200"
+        
+        >
             Django bnb your home
         </div>
     )
