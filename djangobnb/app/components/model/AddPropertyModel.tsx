53e317b6f13a65d6156b4741e49a322b206631c3
Iannaan <moshou5571228@gmail.com>
2024-04-28 23:50:35 +0800

    add choose category

--- djangobnb/app/components/model/AddPropertyModel.tsx
+++ djangobnb/app/components/model/AddPropertyModel.tsx
@@ -7,16 +7,18 @@ import Model from './Model';
 import  useAddPropertyModel from '@/app/hooks/useAddPropertyModel'
 const  AddPropertyModel = () => {
     const addPropertymodel =  useAddPropertyModel();
-
+    const content = (
+        <>
+            <h2 className="mb-6 text-2xl">Choose category </h2>
+        </>
+    )
     return(
         <>
             <Model 
                 isOpen={addPropertymodel.isOpen}
                 close={addPropertymodel.close}
                 label="Add property"
-                content={(
-                    <p>Yo</p>
-                )}
+                content={content}
             />
         </>
     ) 
