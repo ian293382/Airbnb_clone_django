ffbf52c821bbafd0c73210f0055a3f01f8089ee1
Iannaan <moshou5571228@gmail.com>
2024-04-28 23:55:27 +0800

    add currentstep in Addpropertymodel

--- djangobnb/app/components/model/AddPropertyModel.tsx
+++ djangobnb/app/components/model/AddPropertyModel.tsx
@@ -1,15 +1,27 @@
 'use client'
 
+import { useState } from "react";
 import Image from "next/image"
 
 import Model from './Model';
 
 import  useAddPropertyModel from '@/app/hooks/useAddPropertyModel'
+import CustomButton from "../forms/CustomButton";
+
 const  AddPropertyModel = () => {
+    //  設計新增步驟 int step=1
+    const [currentStep, setCurrentStep] = useState(1);
     const addPropertymodel =  useAddPropertyModel();
     const content = (
         <>
             <h2 className="mb-6 text-2xl">Choose category </h2>
+
+            <CustomButton
+                label="Next"
+                onClick={() => 
+                    setCurrentStep(2)
+                }
+            />
         </>
     )
     return(
