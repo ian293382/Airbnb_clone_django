d21d56015d8152071e0484286c37d929362361cd
Iannaan <moshou5571228@gmail.com>
2024-04-29 01:36:44 +0800

    Update category display to clarify user selection

--- djangobnb/app/components/addproperty/Category.tsx
+++ djangobnb/app/components/addproperty/Category.tsx
@@ -1,3 +1,4 @@
+import Image from 'next/image'
 interface CategoriesProps {
     dataCategory: string;
     setCategory: (category: string) => void;
@@ -9,8 +10,81 @@ const Categories: React.FC<CategoriesProps> = ({
 }) => {
     return(
         <>
-            <div className="pt-3 cursor-pointer pt-6 flex item-center space-x-12">
-                asdf
+            <div className="pt-3 cursor-pointer pb-6 flex item-center space-x-12">
+                <div
+                    onClick={()=> setCategory('Arctic')}
+                    className={`pb-4 flex flex-col items-center space-y-2 border-b-2 ${dataCategory === 'Arctic' ? 'border-gray-800' : 'border-white'} opacity-60 hover:border-gray-200 hover:opacity-100`}
+                >
+            <Image
+             src="/category_arctic.jpeg"
+             alt = "Category - Arctic"
+             width={20}
+             height={20}
+            />
+
+            <span className="text-xs">Arctic</span>
+
+                </div>
+
+                <div 
+                    onClick={() => setCategory('Domes')}
+                    className={`pb-4 flex flex-col items-center space-y-2 border-b-2 ${dataCategory === 'Domes' ? 'border-gray-800' : 'border-white'} opacity-60 hover:border-gray-200 hover:opacity-100`}
+                    >
+                    <Image
+                    src="/category_domes.jpeg"
+                    alt = "Category - Domes"
+                    width={20}
+                    height={20}
+                    />
+
+                    <span className="text-xs">Domes</span>
+
+                </div>
+
+                <div 
+                    onClick={() => setCategory('Camping')}
+                    className={`pb-4 flex flex-col items-center space-y-2 border-b-2 ${dataCategory === 'Camping' ? 'border-gray-800' : 'border-white'} opacity-60 hover:border-gray-200 hover:opacity-100`}
+                    >
+                    <Image
+                    src="/category_camping.jpeg"
+                    alt = "Category - Camping"
+                    width={20}
+                    height={20}
+                    />
+
+                    <span className="text-xs">Camping</span>
+
+                </div>
+
+                <div 
+                    onClick={() => setCategory('Top city')}
+                    className={`pb-4 flex flex-col items-center space-y-2 border-b-2 ${dataCategory === 'Top city' ? 'border-gray-800' : 'border-white'} opacity-60 hover:border-gray-200 hover:opacity-100`}
+                    >
+                    <Image
+                        src="/category_top_city.jpeg"
+                        alt = "Category - Top City"
+                        width={20}
+                        height={20}
+                    />
+
+                    <span className="text-xs">Top city</span>
+
+                </div>
+
+                <div 
+                    onClick={() => setCategory('Beachfront')}
+                    className={`pb-4 flex flex-col items-center space-y-2 border-b-2 ${dataCategory === 'Beachfront' ? 'border-gray-800' : 'border-white'} opacity-60 hover:border-gray-200 hover:opacity-100`}
+                    >
+                    <Image
+                        src="/category_beachfront.jpeg"
+                        alt = "Category - Beachfront"
+                        width={20}
+                        height={20}
+                    />
+
+                    <span className="text-xs">Beachfront</span>
+
+                </div>
             </div>
         </>
     )
