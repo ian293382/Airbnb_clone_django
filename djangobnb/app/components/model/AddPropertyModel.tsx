7a4549aeca97d4a30edb2b7107614be9b9dc9408
Iannaan <moshou5571228@gmail.com>
2024-04-29 00:55:56 +0800

    setting  Category content in addpropertymodel page

--- djangobnb/app/components/model/AddPropertyModel.tsx
+++ djangobnb/app/components/model/AddPropertyModel.tsx
@@ -4,20 +4,42 @@ import { useState } from "react";
 import Image from "next/image"
 
 import Model from './Model';
+import CustomButton from "../forms/CustomButton";
+import Categories from "../addproperty/Category";
 
 import  useAddPropertyModel from '@/app/hooks/useAddPropertyModel'
-import CustomButton from "../forms/CustomButton";
 
 const  AddPropertyModel = () => {
-    //  設計新增步驟 int step=1
+    //
+    //  State 設計新增步驟 int step=1
     const [currentStep, setCurrentStep] = useState(1);
+    const [dataCategory, setDataCategory] = useState('');
+
+    //
+    //
+
+
     const addPropertymodel =  useAddPropertyModel();
+    //
+    // Set datas
+    const setCategory = (category: string) => {
+        setDataCategory(category)
+    }
+
+
+    //
+    //
+
+
     const content = (
         <>
             {currentStep == 1? (
                 <>
                     <h2 className="mb-6 text-2xl">Choose category </h2>
-
+                    <Categories
+                        dataCategory={dataCategory}
+                        setCategory={(category) => setCategory(category)}
+                    />
                     <CustomButton
                         label="Next"
                         onClick={() => 
