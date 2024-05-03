651c9dd604da2af9dc80879d156a1f0e84b68960
Iannaan <moshou5571228@gmail.com>
2024-05-04 03:20:50 +0800

    Detailpropertymodel page connect backend data

--- djangobnb/app/properties/[id]/page.tsx
+++ djangobnb/app/properties/[id]/page.tsx
@@ -2,52 +2,61 @@ import Image from "next/image";
 import ReservationSidebar from "@/app/components/properties/ReservationSidebar"
 import React from "react";
 
+import apiService from "@/app/services/apiService";
 
-const PropertyDetailPage = () => {
+
+const PropertyDetailPage = async ({params}: {params: {id: string}}) => {
+    const property = await apiService.get(`/api/properties/${params.id}`)
+    // 在後端上我是並不是直接回傳一個值 而是回傳一個物件 所以  property =>{ data: {}}
+    // 如果你想獲得值 不能寫 property.title 而是 property.data.title
+// const PropertyDetailPage =  () => { // 之前靜態頁面寫法切換可以看比較    
+      
     return (
         <main className="max-w-[1500px] max-auto px-6 pb-6">
-            <div className="w-[100vw] h-[180vh] mb-4 overflow-hidden rounded-xl relative">
+            <div className="w-[100] h-[180vh] mb-4 overflow-hidden rounded-xl relative">
                 <Image 
                     fill
-                    src='/arctic_1.jpg'
-                    alt="arctic_1"
+                    src={property.data.image_url}
+                    alt="property.image"
                     className="object-cover h-full w-full"
 
                 
                 />
             </div>
-
+            
             <div className="mt-4 grid grid-cols-1 md:grid-cols-3 lg:grid-cols-5 gap-4">
                 <div className="py-6 pr-6 col-span-3">
-                    <h1 className="mb-4 text-4xl">Property name</h1>
+                    <h1 className="mb-4 text-4xl">{property.data.title}</h1>
 
                     <span className='mb-6 block text-lg text-gray-600'>
-                        4 gusts - 2 bedrooms - 1 bathroom
+                    {property.data.guests} guests - {property.data.bathrooms} bathrooms  -{property.data.bedrooms} bedrooms
                     </span>
 
                     <hr />
                     <div className="py-6 flex items-center space-x-4">
+                    {property.data.landlord.avatar_url && (
                         <Image 
-                            src="/jerry.jpg"
+                            src= {property.data.landlord.avatar_url}
                             width={50}
                             height={50}
                             className="rounded-full "
                             alt="user name"
                         />
+                    )}
 
-                        <p><strong>John Doe</strong> is your host</p>
+                        <p><strong> {property.data.landlord.name}</strong> is your host</p>
                     </div>
                     <hr />
 
                     <p className="mt-6 text-lg flex justify-center">
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-                        sed do eiusmod tempor incididunt ut labore et dolore magna 
-                        aliqua. Ut enim ad minim veniam, quis nostrud exercitation  
+                     {property.data.description}
                     </p>
                 </div>
 
            
-                <ReservationSidebar />
+                <ReservationSidebar
+                    property={property.data}
+                />
           
             </div>
 
