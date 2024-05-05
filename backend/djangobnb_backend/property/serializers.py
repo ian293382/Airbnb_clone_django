6beaad7434dc66b28a5c72c8222290b7261f89da
Iannaan <moshou5571228@gmail.com>
2024-05-06 01:49:03 +0800

    fix typo

--- backend/djangobnb_backend/property/serializers.py
+++ backend/djangobnb_backend/property/serializers.py
@@ -32,8 +32,8 @@ class PropertiesDetailSerializer(serializers.ModelSerializer):
             'landlord'
         )
 
-class ReservationsListSerializer(serializers.Model):
-    property = PropertiesListSerializer(readonly=True, many=False)
+class ReservationsListSerializer(serializers.ModelSerializer):
+    property = PropertiesListSerializer(read_only=True, many=False)
     class Meta:
         model = Reservation
         fields = (
