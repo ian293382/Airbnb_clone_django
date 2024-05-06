5f0f27503de92700f7f898b41273b486211afba2
Iannaan <moshou5571228@gmail.com>
2024-05-06 15:33:38 +0800

    implement user can click landlord page get backend data

--- djangobnb/app/landlords/[id]/page.tsx
+++ djangobnb/app/landlords/[id]/page.tsx
@@ -2,7 +2,13 @@ import Image from "next/image";
 
 import ContactButton from "@/app/components/ContactButton";
 import PropertyList from "@/app/components/properties/PropertyList";
-const LandlordDetailPage = () => {
+import apiService from "@/app/services/apiService";
+import { getUserId } from "@/app/lib/actions";
+const LandlordDetailPage = async({ params }: {params: {id: string }}) => {
+    // 可以重後端去拿landlord拿資料 呼叫api async await
+    const landlord = await apiService.get(`/api/auth/${params.id}`)
+    const  userId = await getUserId(); 
+
     return (
         <div className="max-w-[1500px] mx-auto px-6 pb-6">
             <div className="grid grid-cols-1 md:grid-cols-4 gap-4">
