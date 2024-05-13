2313fae28e274cc93ad2993c9c4e5d9fd5a4ac54
Iannaan <moshou5571228@gmail.com>
2024-05-14 03:39:26 +0800

    finish search check in step

--- djangobnb/app/components/model/SearchModel.tsx
+++ djangobnb/app/components/model/SearchModel.tsx
@@ -1,14 +1,41 @@
 'use client'
 
 import Model from "./Model";
+import { useState } from "react";
+import  Calendar from '../forms/Calendar'
+import { Range } from 'react-date-range';
+import CustomButton from "../forms/CustomButton";
 import useSearchModel from "@/app/hooks/useSearchModel";
 import SelectCountry, { SelectCountryValue } from "../forms/SelectCountry";
-import { useState } from "react";
+import DatePicker from "../forms/Calendar";
+
+const initialDateRange = {
+    startDate: new Date(),
+    endDate: new Date(),
+    key: 'selection'
+}
 
 const SearchModel = () => {
     let content = (<></>);
     const searchModel = useSearchModel();
+    const [dateRange, setDateRange]= useState<Range>(initialDateRange);
     const [country, setCountry]= useState<SelectCountryValue>();
+    
+    //
+    // Set date range
+
+    const _setDateRange = (selection: Range) => {
+        if (searchModel.step === 'checkin') {
+            searchModel.open('checkout')
+        } else if (searchModel.step === 'checkout') {
+            searchModel.open('details')
+        }
+
+        setDateRange(selection);
+    }
+    
+    //
+    // Contents
 
     const contentLocation = (
         <>
@@ -16,15 +43,49 @@ const SearchModel = () => {
 
             <SelectCountry 
                 value={country}
-                onChange={(value) => setCountry(value as SelectCountryValue)}
+                onChange={(value) => setCountry(value)}
             />
+
+            <div className="mt-6 flex flex-row gap-4">
+                <CustomButton 
+                    label="Check in date ->"
+                    onClick={() =>  searchModel.open('checkin')}
+                />
+
+            </div>
+        </>
+    )
+
+    const contentCheckin = (
+        <>
+            <h2 className="mb-6 text-2xl">When do you want to check in?</h2>
+            <DatePicker
+                value={dateRange}
+                onChange={(value) => _setDateRange(value.selection)}
+            />
+            <div className="mt-6 flex flex-row gap-4">
+                <CustomButton 
+                    label="<- location"
+                    onClick={() =>  searchModel.open('location')}
+                />
+
+                <CustomButton 
+                    label="Check in date ->"
+                    onClick={() =>  searchModel.open('checkout')}
+                />
+
+            </div>
+
         </>
     )
 
     if (searchModel.step == 'location') {
         content = contentLocation;
-
+    }  else if (searchModel.step == 'checkin') {
+        content = contentCheckin;
     }
+
+
     return (
         <Model
             label="Search"
